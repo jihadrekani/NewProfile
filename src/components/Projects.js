7ff5704cec572d12ecce2d96db1d614ec98e2e30
jihadrekani <jihadfed95@gmail.com>
2022-11-21 23:38:38 +0300

    add resturant to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,11 +5,11 @@ export default function Projects() {
   return (
     <div id='projects'>
       <h1 className='font-bold text-3xl mb-12 mt-[250px] text-center  text-yellow-600' >PROJECTS</h1>
-      <div className='  grid  place-items-center xl:grid-cols-2 lg:grid-cols-2  md:grid-cols-2  sm:grid-cols-1   md:space-x-2   '>
+      <div className='  grid  place-items-center xl:grid-cols-2 lg:grid-cols-2  md:grid-cols-2  sm:grid-cols-1   md:space-x-2 md:space-y-4    '>
    
 
 
-      <div className="project-margin max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 ">
+      <div className="project-margin max-w-sm  bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 ">
     <a href="https://jr-cars.netlify.app/">
         <img className="rounded-t-lg h-[250px] w-[400px]" src="https://th.bing.com/th/id/R.e9a636becb4b100ed8eacad468c80568?rik=RUA2cTL4Ko1oMQ&riu=http%3a%2f%2fwww.marinoperformancemotors.com%2fimagetag%2f13302%2f10%2fl%2fUsed-2020-Land-Rover-Range-Rover-Sport-SVR.jpg&ehk=k05jfSTuEIbYTryogfvpdPZT1wWxYhtJZRBmdrUsTus%3d&risl=&pid=ImgRaw&r=0" alt=""/>
     </a>
@@ -36,6 +36,23 @@ export default function Projects() {
         </a>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 direction font-fam">هەمی پەرتوکێن قوتابخانێ و یێن هەمی قوناغا و ب زاراڤێ بادینی و سورانی بەردەستن</p>
         <a href="https://partoken-qotabxane.netlify.app/" target="blank" className="font-fam  inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        ڤەکرن
+            <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"clipRule="evenodd"></path></svg>
+        </a>
+    </div>
+</div>
+
+
+<div className="my-margin max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img className="rounded-t-lg h-[250px] w-[400px]" src="https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt=""/>
+    </a>
+    <div className="p-5">
+        <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center font-fam">خارنگەهـ</h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 direction font-fam">    خوشترین و نویترین خارن لدەف مە هەنە </p>
+        <a href="https://jihad-resturant.netlify.app/" target="blank" className="font-fam  inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         ڤەکرن
             <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"clipRule="evenodd"></path></svg>
         </a>
